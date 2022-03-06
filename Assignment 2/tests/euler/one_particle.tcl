726f25edae8f32e2120aecea72553b8cec5b856d
Kutay Cinar <76612427+kutaycinar@users.noreply.github.com>
2022-03-05 22:05:59 -0800

    Add files via upload

---
+++ Assignment 2/tests/euler/one_particle.tcl
@@ -0,0 +1,12 @@
+system partSys dim 1
+system partSys particle 0 1 0 1 0 0 0 0 
+simulator partSim link partSys 1
+simulator partSim integration euler .01
+
+simulator partSim ground 1000 20
+simulator partSim gravity -9.8
+
+
+
+
+
